05a68e79a18ea21bbacc3b30d7cb7e95bf74c82c
Anupam Sahu <sahuanupam7@gmail.com>
2025-08-04 00:05:13 +0530

    un-templified the vector class
    1. removed constexpr as abs() is not constexpr.
    2. renamed Vec2::Left, Right, Up, Down as West, East, North and South
       respectively to avoid name collisions with Vec3 members.
    3. removed static_asserts as they are useless now.

--- Projects/Core/Include/Core/Vector.h
+++ Projects/Core/Include/Core/Vector.h
@@ -1,57 +1,47 @@
 ﻿#pragma once
+#include <raylib/raylib.h>
 #include <cmath>
 #include <sstream>
 #include <string>
-#include <raylib/raylib.h>
 
 namespace SL
 {
-	template<typename Type>
-	struct TVec2 : Vector2
+	struct Vec2 : Vector2
 	{
-		static_assert(std::is_arithmetic_v<Type>, "TVec2 can only be used with arithmetic types!");
-
 	public:
 
-		constexpr TVec2()
+		constexpr Vec2()
 		{
 			x = 0;
 			y = 0;
 		}
 		
-		constexpr TVec2(const Type XComponent, const Type YComponent)
+		constexpr Vec2(const float XComponent, const float YComponent)
 		{
 			x = XComponent;
 			y = YComponent;
 		}
-
-		template<typename OtherType, typename = std::enable_if<std::is_convertible_v<OtherType, Type>>>
-		constexpr explicit TVec2(const TVec2<OtherType>& Other)
-		{
-			x = static_cast<Type>(Other.x);
-			y = static_cast<Type>(Other.y);
-		}
 	
 	public:
 
-		static const TVec2 Left;
-		static const TVec2 Right;
-		static const TVec2 Up;
-		static const TVec2 Down;
+		static const Vec2 North;
+		static const Vec2 West;
+		static const Vec2 South;
+		static const Vec2 East;
 
 	public:
 		
-		Type GetLength() const { return std::hypot(x, y); }
+		float GetLength() const { return std::hypot(x, y); }
 
-		TVec2 GetNormal(const double Tolerance = 0.00001) const
+		Vec2 GetNormal(const double Tolerance = 0.00001) const
 		{
-			const Type Length = GetLength();
+			const float Length = GetLength();
 			if(Length < Tolerance)
 			{
 				return {0, 0};
 			}
 			
-			return TVec2{x / Length, y / Length};
+			return Vec2{x / Length, y / Length};
 		}
 
 		void Normalize(const double Tolerance = 0.00001)
@@ -59,17 +49,17 @@ namespace SL
 			*this = GetNormal(Tolerance);
 		}
 
-		constexpr Type Dot(const TVec2& Other) const { return x * Other.x + y * Other.y; }
-		constexpr Type Cross(const TVec2& Other) const { return x * Other.y - y * Other.x; }
+		constexpr float Dot(const Vec2& Other) const { return x * Other.x + y * Other.y; }
+		constexpr float Cross(const Vec2& Other) const { return x * Other.y - y * Other.x; }
 		
-		constexpr TVec2 operator+(const TVec2& Other) const { return {x + Other.x, y + Other.y}; }
-		constexpr TVec2 operator-(const TVec2& Other) const { return {x - Other.x, y - Other.y}; }
-		constexpr TVec2 operator-() const { return {-x, -y}; }
+		constexpr Vec2 operator+(const Vec2& Other) const { return {x + Other.x, y + Other.y}; }
+		constexpr Vec2 operator-(const Vec2& Other) const { return {x - Other.x, y - Other.y}; }
+		constexpr Vec2 operator-() const { return {-x, -y}; }
 		
-		constexpr TVec2 operator*(const Type Scalar) const { return {x * Scalar, y * Scalar}; }
-		constexpr friend TVec2 operator*(const Type Scalar, const TVec2& Vector) { return Vector.operator*(Scalar); }
+		constexpr Vec2 operator*(const float Scalar) const { return {x * Scalar, y * Scalar}; }
+		constexpr friend Vec2 operator*(const float Scalar, const Vec2& Vector) { return Vector.operator*(Scalar); }
 
-		constexpr TVec2 operator/(const Type Scalar) const
+		constexpr Vec2 operator/(const float Scalar) const
 		{
 			if(Scalar == 0)
 			{
@@ -78,18 +68,19 @@ namespace SL
 			return {x / Scalar, y / Scalar};
 		}
 
-		constexpr bool operator==(const TVec2 &Other) const
+		bool operator==(const Vec2 &Other) const
 		{
 			return Equals(*this, Other);
 		}
-		static constexpr bool Equals(const TVec2& A, const TVec2& B, const Type Tolerance = 0.00001)
+		
+		static bool Equals(const Vec2& A, const Vec2& B, const float Tolerance = 0.00001)
 		{
-			Type XError = abs(A.x - B.x);
-			Type YError = abs(A.y - B.y);
+			float XError = abs(A.x - B.x);
+			float YError = abs(A.y - B.y);
 			return XError <= Tolerance && YError <= Tolerance;
 		}
 		
-		constexpr bool operator!=(const TVec2& Other) const { return !(*this == Other); }
+		bool operator!=(const Vec2& Other) const { return !(*this == Other); }
 
 		explicit operator std::string() const { return ToString(); }
 		std::string ToString() const
@@ -100,60 +91,42 @@ namespace SL
 		}
 	};
 
-	template<typename Type>
-	const TVec2<Type> TVec2<Type>::Left{-1, 0};
+	const Vec2 North{0, 1};
+	const Vec2 West{-1, 0};
+	const Vec2 South{0, -1};
+	const Vec2 East{1, 0};
 
-	template<typename T>
-	const TVec2<T> TVec2<T>::Right{1, 0};
-
-	template<typename T>
-	const TVec2<T> TVec2<T>::Up{0, 1};
-
-	template<typename T>
-	const TVec2<T> TVec2<T>::Down {0, -1};
-
-	template<typename Type>
-	struct TVec3 : Vector3
+	struct Vec3 : Vector3
 	{
-		static_assert(std::is_arithmetic_v<Type>, "TVec3 can only be used with arithmetic types!");
-
 	public:
 
-		constexpr TVec3()
+		constexpr Vec3()
 		{
 			x = 0;
 			y = 0;
 			z = 0;
 		}
 
-		constexpr TVec3(const Type XComponent, const Type YComponent, const Type ZComponent)
+		constexpr Vec3(const float XComponent, const float YComponent, const float ZComponent)
 		{
 			x = XComponent;
 			y = YComponent;
 			z = ZComponent;
 		}
 		
-		template<typename OtherType, typename = std::enable_if_t<std::is_convertible_v<OtherType, Type>>>
-		constexpr explicit TVec3(const TVec3<OtherType>& Other)
-		{
-			x = static_cast<Type>(Other.x);
-			y = static_cast<Type>(Other.y);
-			z = static_cast<Type>(Other.z);
-		}
-		
 	public:
 
-		static const TVec3 Zero;
-		static const TVec3 Left;
-		static const TVec3 Right;
-		static const TVec3 Up;
-		static const TVec3 Down;
-		static const TVec3 Forward;
-		static const TVec3 Back;
+		static const Vec3 Zero;
+		static const Vec3 Left;
+		static const Vec3 Right;
+		static const Vec3 Up;
+		static const Vec3 Down;
+		static const Vec3 Forward;
+		static const Vec3 Back;
 
 	public:
 
-		static TVec3 Cross(const TVec3& A, const TVec3& B)
+		static constexpr Vec3 Cross(const Vec3& A, const Vec3& B)
 		{
 			return
 			{
@@ -163,22 +136,22 @@ namespace SL
 			};
 		}
 
-		Type GetLength() const { return std::hypot(x, y, z); }
+		float GetLength() const { return std::hypot(x, y, z); }
 
-		TVec3 GetNormal(const double Tolerance = 0.00001) const
+		Vec3 GetNormal(const double Tolerance = 0.00001) const
 		{
-			const Type Length = GetLength();
+			const float Length = GetLength();
 			if(Length < Tolerance)
 			{
 				return {0, 0, 0};
 			}
 			
-			return TVec3{x / Length, y / Length, z / Length};
+			return Vec3{x / Length, y / Length, z / Length};
 		}
 
 		void Normalize(const double Tolerance = 0.00001)
 		{
-			const Type Length = GetLength();
+			const float Length = GetLength();
 			if(Length < Tolerance)
 			{
 				x = y = z = 0;
@@ -190,20 +163,20 @@ namespace SL
 			z /= Length;
 		}
 		
-		constexpr Type Dot(const TVec3& Other) const { return x * Other.x + y * Other.y + z * Other.z; }
+		constexpr float Dot(const Vec3& Other) const { return x * Other.x + y * Other.y + z * Other.z; }
 
-		constexpr TVec3 Cross(const TVec3 &Other) const
+		constexpr Vec3 Cross(const Vec3 &Other) const
 		{
 			return Cross(*this, Other);
 		}
 
-		constexpr TVec3 operator+(const TVec3& Other) const { return {x + Other.x, y + Other.y, z + Other.z}; }
-		constexpr TVec3 operator-(const TVec3& Other) const { return {x - Other.x, y - Other.y, z - Other.z}; }
-		constexpr TVec3 operator-() const { return {-x, -y, -z}; }
+		constexpr Vec3 operator+(const Vec3& Other) const { return {x + Other.x, y + Other.y, z + Other.z}; }
+		constexpr Vec3 operator-(const Vec3& Other) const { return {x - Other.x, y - Other.y, z - Other.z}; }
+		constexpr Vec3 operator-() const { return {-x, -y, -z}; }
 
-		constexpr TVec3 operator*(const Type Scalar) const { return {x * Scalar, y * Scalar, z * Scalar}; }
-		constexpr friend TVec3 operator*(const Type Scalar, const TVec3& Vector) { return Vector.operator*(Scalar);}
-		constexpr TVec3 operator/(const Type Scalar) const
+		constexpr Vec3 operator*(const float Scalar) const { return {x * Scalar, y * Scalar, z * Scalar}; }
+		constexpr friend Vec3 operator*(const float Scalar, const Vec3& Vector) { return Vector.operator*(Scalar);}
+		constexpr Vec3 operator/(const float Scalar) const
 		{
 			if(Scalar == 0)
 			{
@@ -212,19 +185,19 @@ namespace SL
 			return {x / Scalar, y / Scalar, z / Scalar};
 		}
 
-		constexpr bool operator==(const TVec3 &Other) const
+		bool operator==(const Vec3 &Other) const
 		{
 			return Equals(*this, Other);
 		}
-		static constexpr bool Equals(const TVec3& A, const TVec3& B, const Type tolerance = 0.00001)
+		static bool Equals(const Vec3& A, const Vec3& B, const float tolerance = 0.00001)
 		{
-			Type XError = abs(A.x - B.x);
-			Type YError = abs(A.y - B.y);
-			Type ZError = abs(A.z - B.z);
+			float XError = abs(A.x - B.x);
+			float YError = abs(A.y - B.y);
+			float ZError = abs(A.z - B.z);
 			return XError <= tolerance && YError <= tolerance && ZError <= tolerance;
 		}
 		
-		constexpr bool operator!=(const TVec3& Other) const { return !(*this == Other); }
+		bool operator!=(const Vec3& Other) const { return !(*this == Other); }
 		
 		explicit operator std::string() const { return ToString(); }
 		std::string ToString() const
@@ -235,39 +208,11 @@ namespace SL
 		}
 	};
 
-	template<typename T>
-	const TVec3<T> TVec3<T>::Zero{0, 0, 0};
-	
-	template<typename T>
-	const TVec3<T> TVec3<T>::Left{-1, 0, 0};
-
-	template<typename T>
-	const TVec3<T> TVec3<T>::Right{1, 0, 0};
-
-	template<typename T>
-	const TVec3<T> TVec3<T>::Up{0, 0, 1};
-
-	template<typename T>
-	const TVec3<T> TVec3<T>::Down{0, 0, -1};
-
-	template<typename T>
-	const TVec3<T> TVec3<T>::Forward{0, 1, 0};
-
-	template<typename T>
-	const TVec3<T> TVec3<T>::Back{0, -1, 0};
-}
-
-namespace SL
-{
-	// Two-dimensional vector. Type - float.
-	using Vec2 = TVec2<float>;
-	
-	// Two-dimensional vector. Type - double.
-	using Vec2d = TVec2<double>;
-	
-	// Three-dimensional vector. Type - float.
-	using Vec3 = TVec3<float>;
-	
-	// Three-dimensional vector. Type - double.
-	using Vec3d = TVec3<double>;
+	const Vec3 Zero{0, 0, 0};
+	const Vec3 Left{-1, 0, 0};
+	const Vec3 Right{1, 0, 0};
+	const Vec3 Up{0, 0, 1};
+	const Vec3 Down{0, 0, -1};
+	const Vec3 Forward{0, 1, 0};
+	const Vec3 Back{0, -1, 0};
 }
