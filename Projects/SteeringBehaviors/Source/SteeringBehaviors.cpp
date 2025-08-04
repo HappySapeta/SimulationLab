7575c393317f6d00d53884b1782be4deec8e4a46
Anupam Sahu <sahuanupam7@gmail.com>
2025-08-04 18:23:50 +0530

    removed test code.

--- Projects/SteeringBehaviors/Source/SteeringBehaviors.cpp
+++ Projects/SteeringBehaviors/Source/SteeringBehaviors.cpp
@@ -4,11 +4,6 @@
 
 int main()
 {
-    Agent Agent;
-    Agent.SetPosition({0, 0});
-    Agent.SetSize(50.0f);
-
-    float Value = 0.0f;
     InitWindow(SL_WINDOW_WIDTH, SL_WINDOW_HEIGHT, "Steering Behaviors");
     while (!WindowShouldClose())
     {
@@ -16,12 +11,7 @@ int main()
         ClearBackground(LIGHTGRAY);
         BeginDrawing();
         {
-            const float DeltaTime = GetFrameTime();
-            Agent.AddForce({50, Value});
-            Agent.Update(DeltaTime);
-            Agent.Draw();
-
-            Value += DeltaTime * 50;
+           
         }
         EndDrawing();
     }
