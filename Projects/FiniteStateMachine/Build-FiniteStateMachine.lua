a1e3cb4316afd60330ac57ae41bf9f9a9e66934f
Anupam Sahu - SATURN <sahuanupam7@gmail.com>
2025-09-10 13:22:26 +0100

    basic start-up code for the new project

---
+++ Projects/FiniteStateMachine/Build-FiniteStateMachine.lua
@@ -0,0 +1,49 @@
+project "FiniteStateMachine"
+kind "ConsoleApp"
+language "C++"
+cppdialect "C++20"
+targetdir "Binaries/%{cfg.buildcfg}"
+staticruntime "off"
+
+files
+{
+    "Include/**.h",
+    "Source/**.cpp"
+}
+
+includedirs
+{
+    "Include/",
+    "../Core/Include",
+    "../../ThirdParty/Raylib/Include/",
+}
+
+links
+{
+    "raylib",
+    "WinMM",
+	 "Core"
+}
+
+libdirs
+{
+    "../../ThirdParty/Raylib"
+}
+
+targetdir("../Binaries/" .. OutputDir .. "/%{prj.name}")
+objdir("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+
+filter "system:windows"
+systemversion "latest"
+defines { "WINDOWS", "WIN32" }
+
+filter "configurations:Debug"
+defines { "DEBUG" }
+runtime "Debug"
+symbols "On"
+
+filter "configurations:Release"
+defines { "RELEASE" }
+runtime "Release"
+optimize "On"
+symbols "Off"
